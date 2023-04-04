fa145b410361598439fe6fbde0467959f3e34a8e
Przemysław Kaczmarski <nyviel111@gmail.com>
2023-04-04 08:07:59 +0200

    desktop Footer + mobile menu completed

--- main.js
+++ main.js
@@ -1,18 +1,16 @@
 const hamburgerButton = document.querySelector(".mobile-hamburger");
 const primaryHeader = document.querySelector(".primary-header");
-const primaryNavigation = document.querySelector(".primary-navigation");
 const body = document.querySelector("body");
 const iconHamburger = document.querySelector(".icon-hamburger");
 const iconClose = document.querySelector(".icon-close");
 
 hamburgerButton.addEventListener("click", () => {
-	primaryNavigation.toggleAttribute("data-visible");
+	primaryHeader.toggleAttribute("data-visible");
 	hamburgerButton.setAttribute(
 		"aria-expanded",
-		primaryNavigation.hasAttribute("data-visible")
+		primaryHeader.hasAttribute("data-visible")
 	);
-	primaryHeader.toggleAttribute("data-overlay");
-	if (primaryNavigation.hasAttribute("data-visible")) {
+	if (primaryHeader.hasAttribute("data-visible")) {
 		body.style.height = "100%";
 		body.style.overflowY = "hidden";
 		iconClose.style.display = "block";
